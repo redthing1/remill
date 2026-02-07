37d5fafc881f021ad00d825a041cacc70aa76ba4
redthing1 <redthing1@alt.icu>
2026-02-06 18:32:44 -0800

    TraceLifter: handle decode instruction failure

--- lib/BC/TraceLifter.cpp
+++ lib/BC/TraceLifter.cpp
@@ -340,8 +340,13 @@ bool TraceLifter::Impl::Lift(
       inst.Reset();
 
       // TODO(Ian): not passing context around in trace lifter
-      std::ignore = arch->DecodeInstruction(inst_addr, inst_bytes, inst,
-                                            this->arch->CreateInitialContext());
+      if (!arch->DecodeInstruction(inst_addr, inst_bytes, inst,
+                                   this->arch->CreateInitialContext())) {
+        LOG(ERROR) << "Unable to decode instruction bytes at " << std::hex
+                   << inst_addr;
+        AddTerminatingTailCall(block, intrinsics->error, *intrinsics);
+        continue;
+      }
 
       auto lift_status =
           inst.GetLifter()->LiftIntoBlock(inst, block, state_ptr);
